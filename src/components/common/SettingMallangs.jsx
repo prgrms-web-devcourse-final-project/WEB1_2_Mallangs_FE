1b4e36ea6475618d8aebb19870ebcc8b8c7b313a
gd06070 <zizibae0715@naver.com>
2024-12-03 05:44:28 +0900

    ♻️ Refactor: 말랑이 정보 입력 폼 컴포넌트 리팩토링 #44

--- src/components/common/SettingMallangs.jsx
+++ src/components/common/SettingMallangs.jsx
@@ -1,40 +1,30 @@
-import GenderlessIcon from '../../assets/images/icon-genderless.svg?react';
-import MaleIcon from '../../assets/images/icon-male.svg?react';
-import FemaleIcon from '../../assets/images/icon-female.svg?react';
-import InformationIcon from '../../assets/images/icon-information.svg?react';
-import CalendarIcon from '../../assets/images/icon-calendar.svg?react';
-
 import { useState } from 'react';
+
+import GenderSelector from './GenderSelector';
+import ToggleButton from './ToggleButton';
+import Remix from './Remix';
+import InfoText from './InfoText';
+import TypeSelector from './TypeSelector';
 
 // import DatePicker from 'react-datepicker';
 
 const SettingMallangs = () => {
-    const [isSelectedType, setIsSelectedType] = useState(null);
-    const [isSelectedGender, setIsSelectedGender] = useState(null);
-    const [isNeutered, setIsNeutered] = useState(false);
-    const [hasChip, setHasChip] = useState(false);
+    const [isSelectedType, setIsSelectedType] = useState('');
+
+    const handleTypeSelect = (typeId) => {
+        setIsSelectedType(typeId);
+    };
     // const [selectedDate, setSelectedDate] = useState(new Date());
 
+    // const handleDateChange = (date) => {
+    //     setSelectedDate(date);
+    // };
     const mallangTypes = [
         { id: 'dog', label: '강아지' },
         { id: 'cat', label: '고양이' },
         { id: 'bird', label: '새' },
         { id: 'etc', label: '기타' },
     ];
-
-    const genderLabels = {
-        genderless: '중성',
-        male: '남자',
-        female: '여자',
-    };
-
-    // const handleDateChange = (date) => {
-    //     setSelectedDate(date);
-    // };
-
-    const handleGenderSelect = (gender) => {
-        setIsSelectedGender(gender);
-    };
 
     return (
         <div className="setting-mallangs">
@@ -47,22 +37,14 @@ const SettingMallangs = () => {
                     />
                 </div>
 
+                {/* 말랑이 종류 선택 */}
                 <div className="mallang-select-container">
                     <div className="label-type">종류 / 품종</div>
-
-                    <div className="mallang-type-selector">
-                        {mallangTypes.map((type) => (
-                            <div
-                                key={type.id}
-                                className={`filter-label-wrapper ${isSelectedType === type.id ? 'active' : ''}`}
-                                onClick={() => setIsSelectedType(type.id)}
-                            >
-                                <div className={`filter-label-${type.id}`}>
-                                    {type.label}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                    <TypeSelector
+                        types={mallangTypes}
+                        onTypeSelect={handleTypeSelect}
+                        defaultSelectedType="dog"
+                    />
                 </div>
 
                 <div className="form-birthday">
@@ -79,7 +61,7 @@ const SettingMallangs = () => {
 
                         <div className="birthday-wrapper">2024-07-15</div>
                         <div className="birthday-selector">
-                            <CalendarIcon />
+                            <Remix iconName={'calendar-event-line'} />
                         </div>
                     </div>
                 </div>
@@ -99,94 +81,26 @@ const SettingMallangs = () => {
                 </div>
 
                 <div className="form-gender">
-                    <div className="block-label">성별</div>
-
-                    <div className="checkbox-wrapper">
-                        <div
-                            className={`checkbox-group ${isSelectedGender === 'genderless' ? 'active' : ''}`}
-                            onClick={() => handleGenderSelect('genderless')}
-                        >
-                            <div className="frame">
-                                <GenderlessIcon />
-                            </div>
-                        </div>
-                        <div
-                            className={`checkbox-group ${isSelectedGender === 'male' ? 'active' : ''}`}
-                            onClick={() => handleGenderSelect('male')}
-                        >
-                            <div className="frame">
-                                <MaleIcon />
-                            </div>
-                        </div>
-                        <div
-                            className={`checkbox-group ${isSelectedGender === 'female' ? 'active' : ''}`}
-                            onClick={() => handleGenderSelect('female')}
-                        >
-                            <div className="frame">
-                                <FemaleIcon />
-                            </div>
-                        </div>
-
-                        <div className="checkbox-group-label">
-                            {genderLabels[isSelectedGender]}
-                        </div>
-                    </div>
+                    {/* 성별 */}
+                    <GenderSelector />
                 </div>
 
                 <div className="form-input-block">
-                    <div className="block-label">중성화 여부</div>
-
-                    <div className="toggle-div">
-                        <div
-                            className={`toggle-container ${isNeutered ? 'on' : ''}`}
-                            onClick={() => setIsNeutered(!isNeutered)}
-                        >
-                            <div className="toggle-button" />
-                        </div>
-
-                        <div className="toggle-label">
-                            {isNeutered ? '중성화 완료' : '중성화 미완료'}
-                        </div>
-                    </div>
+                    {/* 중성화 여부 */}
+                    <ToggleButton />
                 </div>
 
                 <div className="form-chip">
-                    <div className="block-label">내장 칩 이식 여부</div>
-
-                    <div className="toggle-div">
-                        <div
-                            className={`toggle-container ${hasChip ? 'on' : ''}`}
-                            onClick={() => setHasChip(!hasChip)}
-                        >
-                            <div className="toggle-button" />
-                        </div>
-
-                        <div className="toggle-label">
-                            {hasChip ? '이식함' : '이식하지 않음'}
-                        </div>
-                    </div>
+                    {/* 내장 칩 이식 여부 */}
+                    <ToggleButton
+                        label="내장 칩 이식 여부"
+                        onLabel="이식함"
+                        offLabel="이식하지 않음"
+                    />
                 </div>
 
                 <div className="form-chip-number">
-                    <div className="block-label">칩 인식번호</div>
-
-                    <div className="chip-number-div">
-                        <input
-                            type="number"
-                            placeholder="410020000012345"
-                            className="chip-number"
-                        ></input>
-                    </div>
-
-                    <div className="info">
-                        <div className="info-icon">
-                            <InformationIcon />
-                        </div>
-
-                        <p className="help-text">
-                            인식번호는 어떻게 확인할 수 있나요?
-                        </p>
-                    </div>
+                    <InfoText />
                 </div>
             </div>
         </div>
