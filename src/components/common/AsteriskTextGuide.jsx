78ac15c154c6ea88f934ab75f5056a7819139015
gd06070 <zizibae0715@naver.com>
2024-12-04 22:29:29 +0900

    ♻️ Refactor: 별표 상단 문구 리팩토링 #37

--- src/components/common/AsteriskTextGuide.jsx
+++ src/components/common/AsteriskTextGuide.jsx
@@ -4,9 +4,11 @@ const AsteriskTextGuide = ({
     sectionTitle = '표시된 항목은 필수 입력 항목입니다',
 }) => {
     return (
-        <div className="asterisk-text-guide">
-            <Remix iconName={'asterisk'} />
-            <span className="section-title">{sectionTitle}</span>
+        <div className="asterisk-text">
+            <div className="asterisk-text-guide">
+                <Remix iconName={'asterisk'} />
+                <span className="section-title">{sectionTitle}</span>
+            </div>
         </div>
     );
 };
