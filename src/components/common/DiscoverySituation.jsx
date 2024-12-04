e6a22df92535bbabad5ec4b2da7acbc06f401867
gd06070 <zizibae0715@naver.com>
2024-12-05 03:54:26 +0900

    ♻️ Refactor: 상세설명 textarea 컴포넌트 분리 #37

---
+++ src/components/common/DiscoverySituation.jsx
@@ -0,0 +1,14 @@
+const DiscoverySituation = () => {
+    return (
+        <div className="discovery-situation">
+            <div className="discovery-situation-container">
+                <textarea
+                    className="discovery-situation-container-input"
+                    placeholder="상황 설명 입력..."
+                ></textarea>
+            </div>
+        </div>
+    );
+};
+
+export default DiscoverySituation;
