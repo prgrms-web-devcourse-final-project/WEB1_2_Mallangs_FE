4964194417c9fffa14dd2fbe29c7b43e0cee48ca
gd06070 <zizibae0715@naver.com>
2024-12-17 18:32:32 +0900

    ✨ Feat: 아이디/비밀번호 찾기 api 로직 구현 #90

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -27,3 +27,36 @@ export const logoutApi = async () => {
         throw error;
     }
 };
+
+// 아이디 찾기
+export const findId = async (email, nickname) => {
+    try {
+        const response = await axiosInstance({
+            method: 'post',
+            url: '/member/find-user-id',
+            data: { email, nickname },
+        });
+        console.log('전체 응답:', response);
+        console.log('응답 데이터:', response.data);
+        return response.data;
+    } catch (e) {
+        console.error('아이디 찾기 실패!!', e);
+        throw e;
+    }
+};
+
+// 비밀번호 찾기
+export const findPassword = async (email, userId, title) => {
+    try {
+        const response = await axiosInstance({
+            method: 'post',
+            url: '/member/find-password',
+            data: { email, userId, title },
+        });
+        console.log('응답 데이터:', response.data);
+        return response.data;
+    } catch (e) {
+        console.error('비밀번호 찾기 실패!!', e);
+        throw e;
+    }
+};
